65637199b1251998d64492546ad5c3dd482bdb40
stelmaks <stelmaks101@gmail.com>
2023-01-22 03:31:04 -0800

    What one has not experienced, one will never understand in print

--- main.h
+++ main.h
@@ -28,6 +28,8 @@ struct fmt
 {
 	char fmt;
 	int (*fn)(va_list, char[], int, int, int, int);
+};
+
 
 /**
  * typedef struct fmt fmt_t - Struct op
@@ -35,7 +37,6 @@ struct fmt
  * @fmt: The format.
  * @fm_t: The function associated.
  */
-
 typedef struct fmt fmt_t;
 
 
