a7ac711da5fba8c5849d570c5524fdb0cb94aff2
faju-aoa <unugorfelix@gmail.com>
2023-01-26 06:51:57 -0800

    task 0

--- functions.c
+++ functions.c
@@ -74,35 +74,4 @@ int print_percent(va_list args)
 
 	return (1);
 }
-/************************* PRINT UNSIGNED OCTAL *******************/
-/**
- * print_oct - function that prints unsigned number
- * @args: unsigned number
- * Return: size the output
- */
-int print_oct(va_list args)
-{
-int i;
-int *array;
-int counter = 0;
-unsigned int num = va_arg(args, unsigned int);
-unsigned int temp = num;
-while (num / 8 != 0)
-{
-num /= 8;
-counter++;
-}
-counter++;
-array = malloc(counter *sizeof(int));
-for (i = 0; i < counter; i++)
-{
-array[i] = temp % 8;
-temp /= 8;
-}
-for (i = counter - 1; i >= 0; i--)
-{
-_putchar(array[i] + '0');
-}
-free(array);
-return (counter);
-}
+
--- functions2.c
+++ functions2.c
@@ -126,7 +126,38 @@ i++;
 _putchar(last + '0');
 return (i);
 }
-
+/************************* PRINT UNSIGNED OCTAL *******************/
+/**
+ * print_oct - function that prints unsigned number
+ * @args: unsigned number
+ * Return: size the output
+ */
+int print_oct(va_list args)
+{
+int i;
+int *array;
+int counter = 0;
+unsigned int num = va_arg(args, unsigned int);
+unsigned int temp = num;
+while (num / 8 != 0)
+{
+num /= 8;
+counter++;
+}
+counter++;
+array = malloc(counter *sizeof(int));
+for (i = 0; i < counter; i++)
+{
+array[i] = temp % 8;
+temp /= 8;
+}
+for (i = counter - 1; i >= 0; i--)
+{
+_putchar(array[i] + '0');
+}
+free(array);
+return (counter);
+}
 /********** PRINT UNSIGNED LOWERCASE HEXADECIMAL****************/
 /**
  * print_lx - function that prints unsigned lowercase hexadecimal
